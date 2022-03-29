844b7fdcc5fb65e7873b01e571deae67cc8a8b0a
Vladislav Solomatin <Vladislav.Solomatin@softeq.com>
2022-03-29 14:29:46 +0400

    Add logout handling

---
+++ Presentation/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        public IActionResult SignOut()
+        {
+            return SignOut(new AuthenticationProperties { RedirectUri = "" },
+                CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        public async Task EndSession()
+        {
+            // If AAD sends a single sign-out message to the app, end the user's session, but don't redirect to AAD for sign out.
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        public async Task<IActionResult> SignedOut()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                await EndSession();
+            }
+
+            return Redirect("");
+        }
+    }
+}
